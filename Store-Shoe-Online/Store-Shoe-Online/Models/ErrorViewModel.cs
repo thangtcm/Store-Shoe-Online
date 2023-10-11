3a2d7868deefd37c71441557c8f9b47cd6a343e7
Thắng Epe <caominhthangtran@gmail.com>
2023-10-11 19:39:12 +0700

    First Commit

---
+++ Store-Shoe-Online/Store-Shoe-Online/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Store_Shoe_Online.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
